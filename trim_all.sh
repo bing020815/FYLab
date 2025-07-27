287cef70c6f69d7ab3a0ba84b735565c5a314d61
BingJe <48841696+bing020815@users.noreply.github.com>
2025-07-27 18:20:48 +0800

    Update trim_all.sh

--- trim_all.sh
+++ trim_all.sh
@@ -5,10 +5,16 @@ mkdir -p trimmed_fastq
 FWD_PRIMER="CCTACGGGNGGCWGCAG"
 REV_PRIMER="GACTACHVGGGTATCTAATCC"
 
-for R1 in raw_fastq/*_R1_*.fastq.gz; do
-    R2=${R1/_R1_/_R2_}
+for R1 in raw_fastq/*R1*.fastq.gz; do
+    R2=${R1/R1/R2}
+
+    if [ ! -f "$R2" ]; then
+        echo "Skip: Paired file not found for $R1"
+        continue
+    fi
+
     BASENAME=$(basename "$R1" .fastq.gz)
-    SAMPLE=${BASENAME%_R1_001}
+    SAMPLE=${BASENAME%%_R1*}
 
     OUT_R1="trimmed_fastq/${SAMPLE}_R1_trimmed.fastq.gz"
     OUT_R2="trimmed_fastq/${SAMPLE}_R2_trimmed.fastq.gz"
